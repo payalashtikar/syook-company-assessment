f41b6230ec539a528de347bcca9f95a9b8f98c02
unknown <payalashtikar@gmail.com>
2023-08-29 22:50:29 +0530

    functionality

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose")
 const dburl = 'mongodb+srv://payalashtikar2000:payalashtikar2000@cluster0.8iglqim.mongodb.net/'
+// const dbconnection = mongoose.connection;
+// dbconnection.on("error",console.error.bind(console,"connection failed"));
+// dbconnection.once("open",function(){
+//     console.log("connection successfully")
+// });
 
-mongoose.connect(dburl,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-});
+async function connectDatabase(){
+    try{
+        await mongoose.connect(dburl,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        console.log("Connected")
+    }
+    catch(error){
+        console.error("Error:",error.message)
+    }
+}
 
-const dbconnection = mongoose.connection;
-dbconnection.on("error",console.error.bind(console,"connection failed"));
-dbconnection.once("open",function(){
-    console.log("connection successfully")
-});
-
-module.exports = dbconnection;
+module.exports = connectDatabase;
